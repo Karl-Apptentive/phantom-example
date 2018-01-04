6be94d648693ec9cb5bf1e8fe84b0c2de3fda512
wi101 <wiemzin@gmail.com>
2018-01-04 15:14:09 +0100

    improved code

--- src/main/scala/example/cassandra/tables/Database.scala
+++ src/main/scala/example/cassandra/tables/Database.scala
@@ -3,6 +3,8 @@ package tables
 
 import com.outworkers.phantom.dsl._
 
+import scala.concurrent.Future
+
 object Database extends ExampleDatabase(connector.local)
 
 class ExampleDatabase(override val connector: CassandraConnection)
@@ -10,5 +12,5 @@ class ExampleDatabase(override val connector: CassandraConnection)
 
   object userModel extends UserSchema with Connector
 
-  def saveOrUpdateUser(user: entities.User) = userModel.store(user)
+  def saveOrUpdateUser(user: entities.User): Future[ResultSet] = userModel.store(user).future()
 }
